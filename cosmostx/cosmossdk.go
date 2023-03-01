a4510a597aa4b42d06e4e352abf534083f4520b0
sal_li <sal_li@digifinex.org>
2023-03-01 09:35:28 +0800

    230202:add_fundsdk:submit2

---
+++ cosmostx/cosmossdk.go
@@ -0,0 +1,127 @@
+package cosmostx
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	xauthsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	comostxtypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/shopspring/decimal"
+)
+
+type CosmosSdk struct {
+	FromAdd, ToAdd, Memo, Denom, Value, Fee          string
+	Precision, SigMode                               uint8
+	TimeoutHeight, Sequence, GasLimit, AccountNumber uint64
+	PubkeyByte                                       []byte
+}
+
+func swichSigMode(sigMode uint8) signing.SignMode {
+	if sigMode == 0 {
+		return signing.SignMode_SIGN_MODE_UNSPECIFIED
+	} else if sigMode == 1 {
+		return signing.SignMode_SIGN_MODE_DIRECT
+	} else if sigMode == 2 {
+		return signing.SignMode_SIGN_MODE_TEXTUAL
+	} else {
+		return signing.SignMode_SIGN_MODE_LEGACY_AMINO_JSON
+	}
+}
+
+func (self *CosmosSdk) precisionTransaction(value string, Precision uint8) types.Int {
+	p := decimal.New(1, int32(Precision))
+	v, err := decimal.NewFromString(value)
+	if err != nil {
+		panic(fmt.Errorf("precisionTransaction get value err %v", err))
+	}
+	return types.NewIntFromBigInt(v.Mul(p).BigInt())
+}
+func (self *CosmosSdk) CreateTx(ChainId string, CoinType string) client.TxBuilder {
+	initClientCtx := client.Context{}.WithChainID(ChainId)
+	initClientCtx = initClientCtx.WithTxConfig(simapp.MakeTestEncodingConfig().TxConfig)
+
+	amount := self.precisionTransaction(self.Value, self.Precision)
+	amounttosend, ok := types.NewIntFromString(amount.String())
+	if !ok {
+		panic(fmt.Errorf("create amount err"))
+	}
+	sendMsg := &comostxtypes.MsgSend{FromAddress: self.FromAdd, ToAddress: self.ToAdd, Amount: types.Coins{types.NewCoin(CoinType, amounttosend)}}
+	builder := initClientCtx.TxConfig.NewTxBuilder()
+	builder.SetMsgs(sendMsg)
+	builder.SetGasLimit(self.GasLimit)
+	builder.SetTimeoutHeight(self.TimeoutHeight)
+	builder.SetMemo(self.Memo)
+
+	feeamount := self.precisionTransaction(self.Fee, self.Precision)
+	feeamounttosend, ok := types.NewIntFromString(feeamount.String())
+	builder.SetFeeAmount(types.Coins{types.NewCoin(CoinType, feeamounttosend)})
+	aa := &secp256k1.PubKey{Key: self.PubkeyByte}
+	aa.Type()
+	sig := signing.SignatureV2{
+		PubKey: aa,
+		Data: &signing.SingleSignatureData{
+			SignMode: swichSigMode(self.SigMode),
+		},
+		Sequence: self.Sequence,
+	}
+
+	builder.SetSignatures(sig)
+
+	return builder
+}
+
+func (self *CosmosSdk) CreatTxByte(builder client.TxBuilder, ChainId string) (string, string) {
+	initClientCtx := client.Context{}.WithChainID(ChainId)
+	initClientCtx = initClientCtx.WithTxConfig(simapp.MakeTestEncodingConfig().TxConfig)
+
+	signerData := xauthsigning.SignerData{
+		ChainID:       ChainId,
+		AccountNumber: self.AccountNumber,
+		Sequence:      self.Sequence,
+	}
+	signBytes, err := initClientCtx.TxConfig.SignModeHandler().GetSignBytes(swichSigMode(self.SigMode), signerData, builder.GetTx())
+	if err != nil {
+		fmt.Println(err)
+	}
+	return hex.EncodeToString(crypto.Keccak256Hash(signBytes).Bytes()), hex.EncodeToString(signBytes)
+}
+
+func (self *CosmosSdk) GetTxWithSignature(sign string, builder client.TxBuilder, ChainId string) (string, string) {
+	signBytes, err := hex.DecodeString(sign)
+	if err != nil {
+		fmt.Println(err)
+	}
+	signBytes = signBytes[:64]
+
+	sigData := signing.SingleSignatureData{
+		SignMode:  swichSigMode(self.SigMode),
+		Signature: signBytes,
+	}
+	aa := &secp256k1.PubKey{Key: self.PubkeyByte}
+	sig := signing.SignatureV2{
+		PubKey:   aa,
+		Data:     &sigData,
+		Sequence: self.Sequence,
+	}
+	builder.SetSignatures(sig)
+
+	initClientCtx := client.Context{}.WithChainID(ChainId)
+	initClientCtx = initClientCtx.WithTxConfig(simapp.MakeTestEncodingConfig().TxConfig)
+	txBytes, err := initClientCtx.TxConfig.TxEncoder()(builder.GetTx())
+	txmg, _ := json.Marshal(builder.GetTx())
+	return base64.StdEncoding.EncodeToString(txBytes), hex.EncodeToString(txmg)
+}
+
+func (self *CosmosSdk) PubKey() cryptotypes.PubKey {
+	return &secp256k1.PubKey{
+		Key: self.PubkeyByte,
+	}
+}
